19227037fca926bac5ffa823872a379974322231
JVB Admin <admin@jvbtravel.com>
2026-02-03 13:41:19 +0800

    Removed Add More Clients Toast Modal

--- admin/admin_visa_dashboard.php
+++ admin/admin_visa_dashboard.php
@@ -91,46 +91,5 @@ $visaClients = $visaClientsResult ? $visaClientsResult->fetch_all(MYSQLI_ASSOC) 
     include '../components/add_visa_client.php'; 
   ?>
 
-  <!-- Add Another Group Member Toast Action -->
-  <?php if (isset($_GET['visa_added']) && isset($_SESSION['visa_client_added'])): ?>
-  <div x-data="{ showGroupPrompt: true }" x-show="showGroupPrompt" x-cloak
-       class="fixed bottom-20 right-4 z-40 bg-white rounded-lg shadow-lg border border-gray-200 p-4 max-w-sm">
-    <div class="flex items-start gap-3">
-      <div class="flex-shrink-0">
-        <svg class="w-6 h-6 text-sky-600" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
-        </svg>
-      </div>
-      <div class="flex-1">
-        <p class="text-sm font-medium text-gray-900">Add more members to this group?</p>
-        <p class="text-xs text-gray-500 mt-1">Group Code: <code class="font-mono text-sky-600"><?= htmlspecialchars($_SESSION['visa_client_added']['group_code'] ?? '') ?></code></p>
-        <div class="mt-3 flex gap-2">
-          <button @click="showAddVisaClientModal = true; showGroupPrompt = false"
-                  class="text-xs px-3 py-1.5 bg-sky-600 text-white rounded hover:bg-sky-700 transition-colors">
-            Add Member
-          </button>
-          <a href="admin_visa_dashboard.php"
-             class="text-xs px-3 py-1.5 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 transition-colors">
-            Done
-          </a>
-        </div>
-      </div>
-      <button @click="showGroupPrompt = false; window.location.href='admin_visa_dashboard.php'"
-              class="flex-shrink-0 text-gray-400 hover:text-gray-500">
-        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-        </svg>
-      </button>
-    </div>
-  </div>
-  <?php endif; ?>
-
-  <?php 
-  // Clear session data if not adding another member
-  if (!isset($_GET['visa_added']) && isset($_SESSION['visa_client_added'])) {
-    unset($_SESSION['visa_client_added']);
-  }
-  ?>
-
 </body>
 </html>
